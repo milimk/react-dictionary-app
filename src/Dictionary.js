d62d2f5d6ce818e6b478236cf0ced50c17e1eb12
milimk <milimkim@gmail.com>
2024-09-16 11:48:59 -0700

    Dictionary API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function searchWords(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+
+    let apiKey = "0af4f7ebo6ce11605e35ecb7eatc1716";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleSearch(event) {
